5cc8e18fe81541c0bf02b7456742f425feb725d2
rafysanchez <rafysanchez@hotmail.com>
2018-11-22 14:05:28 -0200

    adjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule, MatTableModule  } from '@angular/material';
+import {  MatPaginatorModule} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatTableModule, MatPaginatorModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatTableModule, MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
